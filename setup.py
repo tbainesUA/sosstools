9bd46c17487e2e888502003a5670f30112ea7153
Tyler Baines <tbaines@stsci.edu>
2024-07-08 15:30:42 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+# # sosstools/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sosstools",
+    version="0.1.0",
+    description="Tools for SOSS data analysis",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+)
